4cdf1d88f59b96beb832f38eb2131b27f8be6c51
sajinael98 <saji.nael.98@gmail.com>
2025-11-09 03:14:32 -0600

    feat: add logo to header

--- src/app/components/layout/Header/Header.tsx
+++ src/app/components/layout/Header/Header.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { AppShellHeader, Group, Button, Anchor, Text } from "@mantine/core";
+import { AppShellHeader, Group, Button, Anchor } from "@mantine/core";
+import Image from "next/image";
 import styles from "./Header.module.css";
 
-const links = ["Home", "Projects", "Skills", "About", "Contact"] as const;
+const links = ["Skills", "Projects", "Contact"] as const;
 
 const Header = () => {
   const navlinks = links.map((l) => (
-    <Anchor key={l} underline="never" href={l} className={styles.navLink}>
+    <Anchor
+      key={l}
+      underline="never"
+      href={`#${l.toLowerCase()}`}
+      className={styles.navLink}
+    >
       {l}
     </Anchor>
   ));
@@ -15,15 +21,13 @@ const Header = () => {
   return (
     <AppShellHeader withBorder={false} className={"glass"}>
       <Group justify="space-between" align="center" px="lg" h="100%">
-        <Text
-          fw={700}
-          fz="xl"
-          variant="gradient"
-          gradient={{ from: "#3A8DFF", to: "#8C52FF", deg: 90 }}
-          style={{ fontFamily: "monospace" }}
-        >
-          &lt;SajiDev /&gt;
-        </Text>
+        <Image
+          src="/logo.png"
+          alt="SajiDev Logo"
+          width={60}
+          height={60}
+          style={{ cursor: "pointer" }}
+        />
 
         <Group gap="lg" visibleFrom="sm">
           {navlinks}
