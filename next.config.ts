c723543b84cfae6ddefff88ad4d0af51f7b47c0a
sajinael98 <saji.nael.98@gmail.com>
2025-11-10 01:12:46 -0600

    fix: remove basePath/assetPrefix for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "portfolio";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
+  basePath: "",
+  assetPrefix: "",
 };
 
 module.exports = nextConfig;
